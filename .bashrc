f9f285df4221a0f9b54d34221f04005f186976ed
Henrique Teixeira <henriquetcro@gmail.com>
2018-09-06 21:05:42 -0300

    Setting up the testing environment

---
+++ .bashrc
@@ -0,0 +1,4 @@
+alias gs="git status"
+alias p="vendor/bin/phpunit"
+alias pf="vendor/bin/phpunit --filter"
+alias github="git push -u origin master"
---
+++ tests/Utilities/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @param $class
+ * @param array $attributes
+ * @return mixed
+ */
+function create($class, $attributes = [], $times = null)
+{
+    return factory($class, $times)->create($attributes);
+}
+
+/**
+ * @param $class
+ * @param array $attributes
+ * @return mixed
+ */
+function make($class, $attributes = [], $times = null)
+{
+    return factory($class, $times)->make($attributes);
+}
